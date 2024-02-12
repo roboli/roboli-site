0a211c9d7e9dffea343e622d3dc2eecd337fe3f2
roboli <robertooliveros@mac.com>
2024-02-12 11:30:31 -0600

    Expand about page

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,1 +1,8 @@
 const screenBreakpoint = 780;
+
+const aboutMe =
+    'With over 12 years of web development experience, I am a highly skilled, versatile, and self-taught '
+    'programmer who can build integrations between apps and systems using serverless technologies. '
+    'I have gained the programming skills to the highest possible level, enhanced by constant research '
+    'and professional development. My education in web development has been 100% commercially focused, '
+    'rather than wasteful theory.';
--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roboli_site/helpers/constants.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -6,8 +7,48 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('About Me'),
+      body: const Padding(
+        padding: EdgeInsets.all(50),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'About Me',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(height: 50,),
+                  Text(
+                    aboutMe,
+                    style: TextStyle(fontSize: 15,),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 100,),
+            Expanded(
+              child: Column(
+                children: [
+                  Text('Skills'),
+                  SizedBox(height: 50,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text('Clojure'),
+                      Text('Dart'),
+                      Text('JS')
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
