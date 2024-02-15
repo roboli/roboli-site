b32e1d6a21dda0e802c0b51801de59e971fab068
roboli <robertooliveros@mac.com>
2024-02-15 04:42:06 -0600

    Add contact form

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,13 +1,128 @@
 import 'package:flutter/material.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  late final TextEditingController _nameController;
+  late final TextEditingController _emailController;
+  late final TextEditingController _commentController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _emailController = TextEditingController();
+    _commentController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _commentController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('Contact Me'),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Contact Me',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 100,),
+                constraints: const BoxConstraints(
+                  minWidth: 500,
+                  maxWidth: 1000,
+                ),
+                child: Form(
+                  child: ListView(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 32,
+                      horizontal: 16,
+                    ),
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0,
+                        ),
+                        child: TextFormField(
+                          controller: _nameController,
+                          decoration: const InputDecoration(
+                            label: Text('Name'),
+                            border: OutlineInputBorder(),
+                          ),
+                          textInputAction: TextInputAction.next,
+                          validator: (value) =>
+                              (value?.isEmpty ?? true) ? 'Name is required' : null,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0,
+                        ),
+                        child: TextFormField(
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                            label: Text('Email'),
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) =>
+                              (value?.isEmpty ?? true) ? 'Email is required' : null,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0,
+                        ),
+                        child: TextFormField(
+                          controller: _commentController,
+                          decoration: const InputDecoration(
+                            label: Text('Email'),
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) =>
+                              (value?.isEmpty ?? true) ? 'Comment is required' : null,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Form.of(context).reset();
+                          },
+                          child: const Text('Reset'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
