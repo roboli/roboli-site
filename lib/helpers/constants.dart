688756312bbeef40f46c56f919f2294f865fb5fb
roboli <robertooliveros@mac.com>
2024-02-22 11:41:29 -0600

    Improve about me text

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -10,25 +10,23 @@ final Map<String, int> pages = {
 const screenWBreakpoint = 920;
 const screenHBreakpoint = 620;
 
-const aboutMe =
-    'With over 12 years of web development experience, I am a highly skilled, versatile, and self-taught '
-    'programmer who can build integrations between apps and systems using serverless technologies. '
-    'I have gained the programming skills to the highest possible level, enhanced by constant research '
-    'and professional development. My education in web development has been 100% commercially focused, '
-    'rather than wasteful theory.';
+const aboutMe = """
+With over 15 years of web development experience, I am a highly skilled, versatile, and self-taught programmer who can build a large variety of applications for any purpose you could imagine. From frontend web or mobile apps to backend systems. Running on dedicated servers or SAAS platforms. Built as a standalone monolith, to independent micro services or even using serverless technologies. I have gained the programming skills to the highest possible level, enhanced by constant research and professional development. My education in web development has been 100% commercially focused.
 
-const myClojure =
-    'Clojure and the JVM are the perfect combination. Clojure as functional language, lets '
-    'you deliver solutions using pragmatic practices. The JVM, a robust platform, has kept the software '
-    'industry running for years and it is stronger than ever.';
+On a personal note, I'm a husband, a father and I have a dog named Ron. In my free time I like to read (and write), watch TV with my kids and continue to practice the piano. I consider myself an introvert, I do exercise whenever I can and strive to never stop learning.
+""";
 
-const myDart =
-    'The internet is everywhere. Having a mobile device is the norm and the days for targeting '
-    'the web as the single platform are no more. Flutter/Dart opens the door to build once and '
-    'deliver everywhere as JS/React cannot never do.';
+const myClojure = """
+Clojure is, as of today, the most modern representation of the Lisp programming languages. With its functional and immutability principles, it is an opinionated language but extremely pragmatic and it won’t get in your way to deliver any kind of solution once you are accustomed to it. It is a hosted language that can run on many platforms but I tend to choose the JVM because it is one of the most robust and stable platforms that ever existed. Proof of it is that it continues to innovate after more than two decades in the industry.
+""";
 
-const myJS =
-    'With more than seven years of NodeJS/React experience, I can delivery any possible solution you could demand.';
+const myDart = """
+The internet is everywhere and having a mobile device is the norm. Long gone are the days where targeting the web as the ubiquitous platform was the only choice. Today, a successful app requires many fronts: web, desktop, mobile or even embedded. But doing so could mean requiring a team for each platform which translates to a lot of money and effort. Enter Flutter and Dart to the rescue. Both technologies together open the door to a “build once and deliver everywhere” multi platform solution. Proof of its reliability comes from the fact that it is used and backed by Google and its popularity has exponentially grown in the last couple of years.
+""";
+
+const myJS = """
+Is a given that JavaScript is the lingua franca of the internet and it will continue to do so for many years. From NodeJS running in the backend to ReactJS for the frontend, I put more than 7 years of my experience at your disposal.
+""";
 
 const jobLH =
     'I built integrations between apps/systems and contributed to the open source Frigg framework '
